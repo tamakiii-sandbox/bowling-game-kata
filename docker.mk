ffa1402713cb8de28f6fc4cb64dded95f7dbd72f
tamakiii <tamakiii@users.noreply.github.com>
2024-04-28 16:27:49 +0900

    init docker.mk

---
+++ docker.mk
@@ -0,0 +1,15 @@
+.PHONY: help build build clean bash
+
+IMAGE := tamakiii-sandbox/bowling-game-kata
+
+help:
+	@cat $(firstword $(MAKEFILE_LIST))
+
+build:
+	docker build -t $(IMAGE) .
+
+clean:
+	docker image rm $(IMAGE)
+
+bash:
+	docker run -it --rm -v $(PWD):/opt/bowling-game-kata $(IMAGE) $@
